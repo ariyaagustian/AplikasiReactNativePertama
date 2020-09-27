5731cc141a8b2003991cd9f735d54aa34fdea92a
ariyaagustian <ariyaagustian@gmail.com>
2020-09-27 07:52:12 +0700

    Basic Styling React Native Component - (Dimensi, Background, Color, Margin, Padding, FontSize)

--- App.js
+++ App.js
@@ -1,17 +1,98 @@
-import React, { Component } from 'react';
-import { Image, Text, TextInput, View } from 'react-native';
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
+  return <StylingReactNativeComponent />;
+};
+
+const StylingReactNativeComponent = () => {
   return (
     <View>
-      <View style={{ width: 80, height: 80, backgroundColor: '#0abde3' }}>
+      <Text style={styles.text}>Styling Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#0abde3',
+          borderWidth: 2,
+          borderColor: '#5f27cd',
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+      <View
+        style={{
+          padding: 12,
+          backgroundColor: '#f2f2f2',
+          width: 212,
+          borderRadius: 8,
+        }}>
+        <Image
+          source={{uri: 'https://placeimg.com/188/107/tech'}}
+          style={{width: 188, height: 107, borderRadius: 8}}
+        />
+        <Text style={{fontSize: 12, fontWeight: 'bold', marginTop: 16}}>
+          New Macbook Pro 2019
+        </Text>
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: 'bold',
+            color: '#f2994A',
+            marginTop: 14,
+          }}>
+          Rp. 25.000.000
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: '300',
+            marginTop: 12,
+          }}>
+          Jakarta Barat
+        </Text>
+        <View
+          style={{
+            backgroundColor: '#6fcf97',
+            paddingVertical: 6,
+            marginTop: 20,
+            borderRadius: 25,
+          }}>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            Beli
+          </Text>
+        </View>
       </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#10ac84',
+    marginLeft: 20,
+    marginTop: 40,
+  },
+});
+
+const SampleComponent = () => {
+  return (
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
       <Text>Ariya</Text>
-      <Agustian></Agustian>
-      <Photo></Photo>
-      <TextInput style={{ borderWidth: 2 }}></TextInput>
-      <BoxGreen></BoxGreen>
-      <Profile></Profile>
+      <Agustian />
+      <Photo />
+      <TextInput style={{borderWidth: 2}} />
+      <BoxGreen />
+      <Profile />
     </View>
   );
 };
@@ -21,27 +102,32 @@ const Agustian = () => {
 };
 
 const Photo = () => {
-  return <Image source={{ uri: 'https://placeimg.com/100/100/animals' }} style={{ width: 100, height: 100 }}></Image>;
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/100/100/animals'}}
+      style={{width: 100, height: 100}}
+    />
+  );
 };
 
 class BoxGreen extends Component {
   render() {
-    return <Text>Component Dari Class</Text>
+    return <Text>Component Dari Class</Text>;
   }
-};
+}
 
 class Profile extends Component {
   render() {
     return (
       <View>
-        <Image source={{ uri: 'https://placeimg.com/100/100/tech' }} style={{ width: 100, height: 100 }}></Image>
+        <Image
+          source={{uri: 'https://placeimg.com/100/100/tech'}}
+          style={{width: 100, height: 100}}
+        />
         <Text>Component Profile</Text>
       </View>
-    )
+    );
   }
 }
-
-
-
 
 export default App;
