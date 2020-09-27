1e970fb9b321c29c3c4dd767d41e54ae518c88e2
ariyaagustian <ariyaagustian@gmail.com>
2020-09-27 07:06:42 +0700

    Membuat Component dengan Class Component di React Native

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Image, Text, TextInput, View } from 'react-native';
 
 const App = () => {
@@ -9,7 +9,9 @@ const App = () => {
       <Text>Ariya</Text>
       <Agustian></Agustian>
       <Photo></Photo>
-      <TextInput style={{borderWidth:2}}></TextInput>
+      <TextInput style={{ borderWidth: 2 }}></TextInput>
+      <BoxGreen></BoxGreen>
+      <Profile></Profile>
     </View>
   );
 };
@@ -17,8 +19,29 @@ const App = () => {
 const Agustian = () => {
   return <Text>Agustian Riyadhi</Text>;
 };
+
 const Photo = () => {
-  return <Image source={{uri:'https://placeimg.com/100/100/animals'}} style={{width:100, height:100}}></Image>;
+  return <Image source={{ uri: 'https://placeimg.com/100/100/animals' }} style={{ width: 100, height: 100 }}></Image>;
 };
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Component Dari Class</Text>
+  }
+};
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={{ uri: 'https://placeimg.com/100/100/tech' }} style={{ width: 100, height: 100 }}></Image>
+        <Text>Component Profile</Text>
+      </View>
+    )
+  }
+}
+
+
+
 
 export default App;
